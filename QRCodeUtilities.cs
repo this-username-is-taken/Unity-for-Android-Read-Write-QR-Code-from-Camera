a3b8e1292029a57b31d63eb23fe302b3455bcd08
Guidanel <guidanel@gmail.com>
2017-06-11 11:28:12 -0400

    Create QRCodeUtilities.cs

--- QRCodeUtilities.cs
+++ QRCodeUtilities.cs
@@ -9,14 +9,18 @@ public static class QRCodeUtilities {
 	private static readonly Color32 white = new Color32(255, 255, 255, 255);
 	private static readonly Color32 black = new Color32(0, 0, 0, 255);
 
-	public static Texture2D GenerateQRCode(string text) {
+	// Generates a Texture2D that can be displayed in a RawImage
+	// Works better (for me) on Android than the built in methods of ZXing
+	public static Texture2D GenerateQRCode(string text, int quietZoneSize) {
+		// The white area around the QR Code. Usually 4 units is the minimum
+		quietZoneSize = Mathf.Max(0, quietZoneSize);
+		
 		// Step 1 - generate the QRCode dot array
 		Dictionary<EncodeHintType, object> hints = new Dictionary<EncodeHintType, object>();
 		hints.Add(EncodeHintType.CHARACTER_SET, "UTF-8");
 		QRCode qrCode = Encoder.encode(text, ErrorCorrectionLevel.L, hints);
 
 		// Step 2 - create a Texture2D out of this array
-		int quietZoneSize = 5;
 		int width = qrCode.Matrix.Width + 2 * quietZoneSize;
 		int height = qrCode.Matrix.Height + 2 * quietZoneSize;
 		int qrCodeStartX = quietZoneSize;
@@ -43,6 +47,7 @@ public static class QRCodeUtilities {
 	}
 
 	public static Result DecodeQRCode(Texture2D texture2D) {
+		// Define the reader
 		BarcodeReader barcodeReader = new BarcodeReader();
 		List<BarcodeFormat> possibleFormats = new List<BarcodeFormat>();
 		possibleFormats.Add(BarcodeFormat.QR_CODE);
@@ -54,7 +59,7 @@ public static class QRCodeUtilities {
 
 		Result result = barcodeReader.Decode(texture2D.GetPixels32(), texture2D.width, texture2D.height);
 		if (result == null) {
-			// Try rotating the image. I know that QR code don't care about the orientation, but without this the scan fails often
+			// Try rotating the image. I know that QR codes don't care about orientation, but without this the scan fails often
 			texture2D = RotateTexture(texture2D);
 			result = barcodeReader.Decode(texture2D.GetPixels32(), texture2D.width, texture2D.height);
 		}
@@ -65,7 +70,6 @@ public static class QRCodeUtilities {
 	private static Texture2D RotateTexture(Texture2D image) {
 		//flip image width<>height, as we rotated the image, it might be a rect. not a square image
 		Texture2D target = new Texture2D(image.height, image.width, image.format, false);    
-
 		Color32[] pixels = image.GetPixels32(0);
 		pixels = RotateTextureGrid(pixels, image.width, image.height);
 		target.SetPixels32(pixels);
